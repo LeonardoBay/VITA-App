180d84ed4434f99aae29282ec616610aa85152cc
nicolebruch <bruchnicole@gmail.com>
2024-02-11 18:07:15 -0300

    Teste e alteração tab1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+//começar a importar daqui os meus arquivos p/ fazer as rotas de navegaçao
+
+//importa {nomeDoArquivo da tela} para a "URL", no caso especifico as pastas
+//de onde se localiza o meu arquivo da tela
+
+
+//aqui sao as rotas
 const routes: Routes = [
   {
     path: '',
@@ -14,3 +21,9 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
+
+//canActivate: [ApiGuard] --> É uma api p/ usuário se manter
+//logado na tela, mesmo que ele saia do app
+
+//ApiGuard cria regra --> Só pode ficar/se manter na tela se retornar TRUE
